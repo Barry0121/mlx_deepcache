136a1e7b3a5a21a9c43f1e8124293874459c5a69
Barry Xue <66921377+Barry0121@users.noreply.github.com>
2024-03-18 14:58:23 -0400

    (WIP) Implement MLXDeepCache: Analysis of MLX SD with comments

--- mlx_deepcache/txt2image.py
+++ mlx_deepcache/txt2image.py
@@ -1,10 +1,10 @@
 # Copyright © 2023 Apple Inc.
 
 import argparse
-import math
-from mlx.nn import QuantizedLinear
+
 import mlx.core as mx
 import numpy as np
+from mlx.nn import QuantizedLinear
 from PIL import Image
 from tqdm import tqdm
 
@@ -12,23 +12,37 @@ from stable_diffusion import StableDiffusion, StableDiffusionXL
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Generate images from an image and a textual prompt using stable diffusion"
+        description="Generate images from a textual prompt using stable diffusion"
     )
-    parser.add_argument("image")
     parser.add_argument("prompt")
     parser.add_argument("--model", choices=["sd", "sdxl"], default="sdxl")
-    parser.add_argument("--strength", type=float, default=0.9)
     parser.add_argument("--n_images", type=int, default=4)
     parser.add_argument("--steps", type=int)
     parser.add_argument("--cfg", type=float)
     parser.add_argument("--negative_prompt", default="")
     parser.add_argument("--n_rows", type=int, default=1)
     parser.add_argument("--decoding_batch_size", type=int, default=1)
-    parser.add_argument("--quantize", "-q", action="store_true")
     parser.add_argument("--no-float16", dest="float16", action="store_false")
+    parser.add_argument("--quantize", "-q", action="store_true")
     parser.add_argument("--preload-models", action="store_true")
     parser.add_argument("--output", default="out.png")
+    parser.add_argument("--seed", type=int)
     parser.add_argument("--verbose", "-v", action="store_true")
+
+    # DeepCache Hyperparameter
+    parser.add_argument("--cache_interval", type=int,
+                        help="N, number of interval to use the cached output.")
+    parser.add_argument("--cache_layer_id", type=int,
+                        help="m, unet branch to apply our cache.")
+    parser.add_argument("--cache_block_id", type=int,
+                        help="Id of the up-sampling block to cache.")
+    parser.add_argument("--uniform", type=bool,
+                        help="Sampling strategy for N step caching.")
+    parser.add_argument("--center", type=int,
+                        help="Center timestep of the frequency distribution.")
+    parser.add_argument(
+        "--pow", type=float, help="Power value to determine the shape of the frequency distribution.")
+
     args = parser.parse_args()
 
     # Load the models
@@ -50,41 +64,20 @@ if __name__ == "__main__":
         args.cfg = args.cfg or 7.5
         args.steps = args.steps or 50
 
-    # Fix the steps if they were set too low
-    if int(args.steps * args.strength) < 1:
-        args.steps = int(math.ceil(1 / args.strength))
-        if args.verbose:
-            print(
-                f"Strength {args.strength} is too low so steps were set to {args.steps}"
-            )
-
     # Ensure that models are read in memory if needed
     if args.preload_models:
         sd.ensure_models_are_loaded()
 
-    # Read the image
-    img = Image.open(args.image)
-
-    # Make sure image shape is divisible by 64
-    W, H = (dim - dim % 64 for dim in (img.width, img.height))
-    if W != img.width or H != img.height:
-        print(f"Warning: image shape is not divisible by 64, downsampling to {W}x{H}")
-        img = img.resize((W, H), Image.NEAREST)  # use desired downsampling filter
-
-    img = mx.array(np.array(img))
-    img = (img[:, :, :3].astype(mx.float32) / 255) * 2 - 1
-
-    # Noise and denoise the latents produced by encoding the img.
-    latents = sd.generate_latents_from_image(
-        img,
+    # Generate the latent vectors using diffusion
+    latents = sd.generate_latents(
         args.prompt,
-        strength=args.strength,
         n_images=args.n_images,
         cfg_weight=args.cfg,
         num_steps=args.steps,
+        seed=args.seed,
         negative_text=args.negative_prompt,
     )
-    for x_t in tqdm(latents, total=int(args.steps * args.strength)):
+    for x_t in tqdm(latents, total=args.steps):
         mx.eval(x_t)
 
     # The following is not necessary but it may help in memory
@@ -102,7 +95,7 @@ if __name__ == "__main__":
     # Decode them into images
     decoded = []
     for i in tqdm(range(0, args.n_images, args.decoding_batch_size)):
-        decoded.append(sd.decode(x_t[i : i + args.decoding_batch_size]))
+        decoded.append(sd.decode(x_t[i: i + args.decoding_batch_size]))
         mx.eval(decoded[-1])
     peak_mem_overall = mx.metal.get_peak_memory() / 1024**3
 
@@ -110,7 +103,8 @@ if __name__ == "__main__":
     x = mx.concatenate(decoded, axis=0)
     x = mx.pad(x, [(0, 0), (8, 8), (8, 8), (0, 0)])
     B, H, W, C = x.shape
-    x = x.reshape(args.n_rows, B // args.n_rows, H, W, C).transpose(0, 2, 1, 3, 4)
+    x = x.reshape(args.n_rows, B // args.n_rows,
+                  H, W, C).transpose(0, 2, 1, 3, 4)
     x = x.reshape(args.n_rows * H, B // args.n_rows * W, C)
     x = (x * 255).astype(mx.uint8)
 
